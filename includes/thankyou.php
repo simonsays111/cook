03531c5dd7fc5de99f0fd107627e0ab51bd489c2
Simon <shimi54@gmail.com>
2018-01-14 00:04:28 +0200

    Revert "1"
    This reverts commit 0b9a324b82ad25b31b810c1502b39f385490060d.

---
+++ includes/thankyou.php
@@ -0,0 +1,29 @@
+<?php
+require 'connection.php';
+$conn    = Connect();
+
+
+$f_name = mysqli_real_escape_string($conn, $_POST['f_name']);
+$l_name = mysqli_real_escape_string($conn, $_POST['l_name']);
+$u_email = mysqli_real_escape_string($conn, $_POST['u_email']);
+$u_name = mysqli_real_escape_string($conn, $_POST['u_name']);
+$gender = mysqli_real_escape_string($conn, $_POST['gender']);
+$date = mysqli_real_escape_string($conn, $_POST['date']);
+$time = mysqli_real_escape_string($conn, $_POST['time']);
+$place = mysqli_real_escape_string($conn, $_POST['place']);
+
+ 
+$sql = "INSERT INTO tb_cform (f_name, l_name, u_email,u_name,gender,date,time,place) VALUES ('$f_name', '$l_name', '$u_email','$u_name','$gender','$date','$time','$place')";
+ 
+if($conn->query($sql) === TRUE){
+ include 'thankyou.html';
+}
+else
+{
+ include 'failedForm.html';
+
+}
+$conn->close();
+?>
+
+
