5dcce29b339a9f8529012298174b9e998372fcef
michalimat <33857446+michalimat@users.noreply.github.com>
2018-01-13 15:53:39 +0200

    checking-michal

--- includes/form.php
+++ includes/form.php
@@ -5,7 +5,7 @@
     <head> 
         
         
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="../CSS/map.css">
 
 
